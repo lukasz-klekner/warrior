4a23e252bb825a461a320f03bac5e8c9d02a3c87
Luki Klekner <lukaszklekner@gmail.com>
2023-02-17 15:50:35 +0100

    feat: add enum ActiveWarrior

--- Arena.ts
+++ Arena.ts
@@ -1,26 +1,22 @@
 import { Warrior } from "./Warrior";
 
+enum ActiveWarrior {
+  First,
+  Second
+}
+
   
   export class Arena {
-    private activeWarrior: number;
+    activeWarrior: ActiveWarrior.First | ActiveWarrior.Second = ActiveWarrior.Second;
 
     constructor(public warrior1: Warrior,public warrior2: Warrior) {
-      if (!(warrior1 instanceof Warrior)) {
-        throw new Error('warrior1 must be an instance of Warrior class!');
-      }
-  
-      if (!(warrior2 instanceof Warrior)) {
-        throw new Error('warrior2 must be an instance of Warrior class!');
-      }
-  
       this.warrior1 = warrior1;
       this.warrior2 = warrior2;
-      this.activeWarrior = 2;
     }
   
     fight(): Warrior | null {
-      const attacker = this.activeWarrior === 1 ? this.warrior1 : this.warrior2;
-      const attacked = this.activeWarrior === 1 ? this.warrior2 : this.warrior1;
+      const attacker = this.activeWarrior === ActiveWarrior.First ? this.warrior1 : this.warrior2;
+      const attacked = this.activeWarrior === ActiveWarrior.First ? this.warrior2 : this.warrior1;
   
       const attackingHitPoints = attacker.hitPoints;
       const attackedOldHp = attacked.hp;
@@ -30,14 +26,7 @@ import { Warrior } from "./Warrior";
   
       attacked.hp = attackedNewHp;
   
-      this.activeWarrior = this.activeWarrior === 1 ? 2 : 1;
-      /**
-        if (this.activeWarrior === 1) {
-          this.activeWarrior = 2;
-        } else {
-          this.activeWarrior = 1;
-        }
-       */
+      this.activeWarrior = this.activeWarrior === ActiveWarrior.First ? ActiveWarrior.Second : ActiveWarrior.First;
   
       if (attackedNewHp <= 0) {
         console.log(attacked.name, 'goes to Valhalla');
